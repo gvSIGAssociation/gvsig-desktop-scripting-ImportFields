a3cfc98880211d63eb0653c64c40b5e93cb66833
jjdelcerro <jjdelcerro@administrador-Lenovo-G50-70>
2019-12-27 13:39:35 +0100

    Renombrado de variables, llamandos target y source en lugar de 1 y 2

--- importFieldsLib.py
+++ importFieldsLib.py
@@ -28,108 +28,108 @@ def main(*args):
     
     processImportFields(table1, field1, table2, field2, data)
     
-def processImportFields(table1, field1, table2, field2, data, taskStatus=None):
+def processImportFields(tableTarget, fieldTarget, tableSource, fieldSource, data, taskStatus=None):
   ### Init processimportfields"
   # Update ft
-  store1 = table1.getFeatureStore()
-  store2 = table2.getFeatureStore()
-  ft1 = store1.getDefaultFeatureType()
-  ft2 = store2.getDefaultFeatureType()
-  if store1==store2:
+  storeTarget = tableTarget.getFeatureStore()
+  storeSource = tableSource.getFeatureStore()
+  ftTarget = storeTarget.getDefaultFeatureType()
+  ftSource = storeSource.getDefaultFeatureType()
+  if storeTarget==storeSource:
     logger("Can't use same store for import tables", LOGGER_ERROR)
     return
   # Checks
   ## Ningun del nombre de los campos nuevos usados existe ya en la tabla previa
   for d in data:
-    used = ft1.getAttributeDescriptor(d['idname'])
+    used = ftTarget.getAttributeDescriptor(d['idname'])
     if used != None:
       logger("Field "+d['idname'] + " already created in table", LOGGER_ERROR)
       return
 
   # Process
-  newft1 = gvsig.createFeatureType(ft1)
-  newft2 = gvsig.createFeatureType(ft2)
+  newftTarget = gvsig.createFeatureType(ftTarget)
+  newftSource = gvsig.createFeatureType(ftSource)
 
   for d in data:
     name = d["idfield"]
-    descriptor = newft2.getEditableAttributeDescriptor(name)
+    descriptor = newftSource.getEditableAttributeDescriptor(name)
     newname = d["idname"]
     descriptor.setName(newname)
-    newft1.addLike(descriptor)
+    newftTarget.addLike(descriptor)
 
   
   try:
-    store1.edit()
-    store1.update(newft1)
-    store1.commit()
+    storeTarget.edit()
+    storeTarget.update(newftTarget)
+    storeTarget.commit()
   except:
     logger("Not able to update feature type", LOGGER_ERROR)
     return
 
   
   # Update values
-  store1.edit()
-  fset2 = table2.getFeatureStore().getFeatureSet()
+  storeTarget.edit()
+  fsetSource = tableSource.getFeatureStore().getFeatureSet()
   
   if taskStatus != None:
-    taskStatus.setRangeOfValues(0, fset2.getSize())
+    taskStatus.setRangeOfValues(0, fsetSource.getSize())
     taskStatus.setCurValue(0)
     taskStatus.add()
 
-  for f2 in fset2:
+  for fSource in fsetSource:
     if taskStatus!=None and taskStatus.isCancellationRequested():
       break
     if taskStatus != None:
       taskStatus.incrementCurrentValue()
     # QUERY
-    builder = store1.createExpressionBuilder()
+    builder = storeTarget.createExpressionBuilder()
     ## Eq expression
     expFilter = builder.eq(
-          builder.column(field1),
-          builder.constant(f2.get(field2))
+          builder.column(fieldTarget),
+          builder.constant(fSource.get(fieldSource))
           ).toString()
 
     exp = ExpressionEvaluatorLocator.getManager().createExpression()
     exp.setPhrase(expFilter)
     evaluator = DALLocator.getDataManager().createExpresion(exp)
-    #filterValue = str(field2)+"="+str(f1.get(field1))
-    fq1 = store1.createFeatureQuery()
-    fq1.setFilter(evaluator)
+    #filterValue = str(field2)+"="+str(fTarget.get(fieldTarget))
+    fqTarget = storeTarget.createFeatureQuery()
+    fqTarget.setFilter(evaluator)
     
     #for d in data: #need al attributos to get editable valid
-    #  if not ft2.getAttributeDescriptor(d["idfield"]).isReadOnly():
+    #  if not ftSource.getAttributeDescriptor(d["idfield"]).isReadOnly():
     #    fq1.addAttributeName(d["idname"])
-    #defaultGeometry =  ft1.getDefaultGeometryAttributeName()
-    #fq1.addAttributeName(defaultGeometry)
-    fq1.retrievesAllAttributes()
+    #defaultGeometry =  ftTarget.getDefaultGeometryAttributeName()
+    #fqTarget.addAttributeName(defaultGeometry)
+    fqTarget.retrievesAllAttributes()
     
     # UPDATE VALUES
-    fset1 = store1.getFeatureSet(fq1)
-    for f1 in fset1:
-      ef1 = f1.getEditable()
+    fsetTarget = storeTarget.getFeatureSet(fqTarget)
+    for fSource in fsetSource:
+      efTarget = ftTarget.getEditable()
       for d in data:
-        if newft1.getAttributeDescriptor(d["idname"]).isReadOnly():
+        if newftTarget.getAttributeDescriptor(d["idname"]).isReadOnly():
           #logger("Field is read only", LOGGER_WARN) 
           continue
-        value = f2.get(d["idfield"])
-        ef1.set(d["idname"],value)
-      fset1.update(ef1)
-    if store1.canCommitChanges() and store1.getPendingChangesCount()>100000:
-      store1.commitChanges()
-  store1.commit()
+        value = fSource.get(d["idfield"])
+        efTarget.set(d["idname"],value)
+      fsetTarget.update(efTarget)
+    if storeTarget.canCommitChanges() and storeTarget.getPendingChangesCount()>100000:
+      storeTarget.commitChanges()
+  storeTarget.commit()
   try:
     pass
   except:
     ex = sys.exc_info()[1]
     logger("Not able to update features value: " + str(ex), LOGGER_ERROR)
     try:
-      DisposeUtils.disposeQuietly(fset1)
-      DisposeUtils.disposeQuietly(fset2)
+      DisposeUtils.disposeQuietly(fsetTarget)
+      DisposeUtils.disposeQuietly(fsetSource)
       store1.cancelEditing()
     except:
       logger("Not able to cancel editing", LOGGER_ERROR)
   finally:
-    DisposeUtils.disposeQuietly(fset1)
-    DisposeUtils.disposeQuietly(fset2)
+    DisposeUtils.disposeQuietly(fsetTarget)
+    DisposeUtils.disposeQuietly(fsetSource)
   
   
